97e8a5131fd4644ab0edfa4b2a04e1205ea4abbd
Murat Mukhtarov <mmukhtarov@zendesk.com>
2016-07-20 20:47:22 +1000

    Change config and mount volume

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --name bagpipe -dti --cap-add=NET_ADMIN --net host bagpipe-bgp  bash -c "service bagpipe-bgp start"
+docker run --name bagpipe -dti --cap-add=NET_ADMIN -v ./bgp.conf:/etc/bagpipe-bgp/bgp.conf -p 127.0.0.1:8082:8082  bagpipe-bgp  bash -c "service bagpipe-bgp start"
